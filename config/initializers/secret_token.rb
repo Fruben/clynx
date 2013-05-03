d6006b4ac3116effbe5ae9df0e3e978c902e711d
Ryan Flournoy <ryanflournoy@yahoo.com>
2013-05-03 14:25:14 -0700

    Construction lynx proto type started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clynx::Application.config.secret_token = 'f7471edea0ee1e0f5d06daa95fe760f775fcc5a282df30feb156112a3e10fd4e4919c2c822ebd9926e849dea6314beada617e0bebf804a96aafdbf63cd355980'
